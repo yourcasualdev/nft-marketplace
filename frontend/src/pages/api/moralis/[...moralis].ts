2a7d0e744855b2d404743056848ea532699ecb25
İbrahim BABAL <ibrahim.babal@anycivilization.com>
2022-12-21 17:25:23 +0300

    test

--- frontend/src/pages/api/moralis/[...moralis].ts
+++ frontend/src/pages/api/moralis/[...moralis].ts
@@ -4,7 +4,7 @@ export default MoralisNextApi({
     apiKey: process.env.MORALIS_API_KEY || '',
     authentication: {
         domain: process.env.VERCEL_URL || process.env.NEXT_AUTH_URL || 'http://localhost:3000',
-        uri: process.env.VERCEL_URL || process.env.NEXT_AUTH_URL || 'http://localhost:3000',
+        uri: process.env.VERCEL_URL || `https://${process.env.NEXT_AUTH_URL}` || 'http://localhost:3000',
         timeout: 120,
     },
 });
