e95b366bd951a285e3191615edbd16240cf6900a
İbrahim BABAL <ibrahim.babal@anycivilization.com>
2022-12-21 17:27:36 +0300

    next auth fix

--- frontend/src/pages/api/moralis/[...moralis].ts
+++ frontend/src/pages/api/moralis/[...moralis].ts
@@ -3,8 +3,8 @@ import { MoralisNextApi } from '@moralisweb3/next';
 export default MoralisNextApi({
     apiKey: process.env.MORALIS_API_KEY || '',
     authentication: {
-        domain: process.env.VERCEL_URL || process.env.NEXT_AUTH_URL || 'http://localhost:3000',
-        uri: process.env.VERCEL_URL || `https://${process.env.NEXT_AUTH_URL}` || 'http://localhost:3000',
+        domain: process.env.NEXTAUTH_URL || `https://${process.env.VERCEL_URL}` || 'http://localhost:3000',
+        uri: process.env.NEXTAUTH_URL || `https://${process.env.VERCEL_URL}` || 'http://localhost:3000',
         timeout: 120,
     },
 });
